4f04cfaa280c7cbd2f3a420dc24addc2b5436436
Piotr Napieralski <piotrn-87@o2.pl>
2019-12-23 21:06:33 +0100

    Feat: Initial commit

---
+++ src/Start.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Start extends Component {
+  render() {
+    return <h1>Hello Amanda. I want to play a game ... </h1>;
+  }
+}
+export default Start;
