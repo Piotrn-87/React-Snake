41249520fd41573399349845cfd6355d8bf644d3
Piotr Napieralski <piotrn-87@o2.pl>
2020-01-18 16:29:53 +0100

    Updates

---
+++ precache-manifest.0958c1e7ee5e8b468f75ae97da883e7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92343752419c15883ae05f195b8c7ee",
+    "url": "/React-Snake/index.html"
+  },
+  {
+    "revision": "65bbfb39d418dd9f87c5",
+    "url": "/React-Snake/static/css/main.17bd64c8.chunk.css"
+  },
+  {
+    "revision": "144995991af688f45dbf",
+    "url": "/React-Snake/static/js/2.6c5b77f8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Snake/static/js/2.6c5b77f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65bbfb39d418dd9f87c5",
+    "url": "/React-Snake/static/js/main.88c0e982.chunk.js"
+  },
+  {
+    "revision": "020af7ed164cffc0d3d0",
+    "url": "/React-Snake/static/js/runtime-main.e0bc239e.js"
+  }
+]);
