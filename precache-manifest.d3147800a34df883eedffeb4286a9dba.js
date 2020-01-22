f4987760f65914955003fe99d30473288a67b155
Piotr Napieralski <piotrn-87@o2.pl>
2020-01-22 18:43:32 +0100

    Updates

--- precache-manifest.d3147800a34df883eedffeb4286a9dba.js
+++ precache-manifest.d3147800a34df883eedffeb4286a9dba.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ee0bbf7a7a9f917fe1a47b520c08e54e",
+    "revision": "f8d2e6047f3ecabbe8e764f73f505b61",
     "url": "/React-Snake/index.html"
   },
   {
-    "revision": "0619cc690ba85cc45d9f",
-    "url": "/React-Snake/static/css/main.0c244270.chunk.css"
+    "revision": "81af3568c1c5046229ac",
+    "url": "/React-Snake/static/css/main.da20e37e.chunk.css"
   },
   {
     "revision": "144995991af688f45dbf",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Snake/static/js/2.6c5b77f8.chunk.js.LICENSE"
   },
   {
-    "revision": "0619cc690ba85cc45d9f",
-    "url": "/React-Snake/static/js/main.b3253612.chunk.js"
+    "revision": "81af3568c1c5046229ac",
+    "url": "/React-Snake/static/js/main.c45fca58.chunk.js"
   },
   {
     "revision": "020af7ed164cffc0d3d0",
